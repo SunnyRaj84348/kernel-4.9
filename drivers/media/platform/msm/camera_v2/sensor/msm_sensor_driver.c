91e04079b24bf68d2f4d863e06abbe34b4a1b9bc
LuK1337 <priv.luk@gmail.com>
2018-03-23 14:30:42 +0530

    camera_v2: Revert "msm: camera: Bypass redundant creation of video node in stereo"
    This reverts commit 3d5e5360d2aa9cacd2848cb461450c3a00c5dca9.
    
    Change-Id: I54dd567763b7130653e3b0148398cdcad670ab3
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -87,13 +87,17 @@ static int32_t msm_sensor_driver_create_i2c_v4l_subdev
 	struct i2c_client *client = s_ctrl->sensor_i2c_client->client;
 
 	CDBG("%s %s I2c probe succeeded\n", __func__, client->name);
+#ifndef CONFIG_MACH_XIAOMI_RIVA
 	if (s_ctrl->bypass_video_node_creation == 0) {
+#endif
 		rc = camera_init_v4l2(&client->dev, &session_id);
 		if (rc < 0) {
 			pr_err("failed: camera_init_i2c_v4l2 rc %d", rc);
 			return rc;
 		}
+#ifndef CONFIG_MACH_XIAOMI_RIVA
 	}
+#endif
 
 	CDBG("%s rc %d session_id %d\n", __func__, rc, session_id);
 	snprintf(s_ctrl->msm_sd.sd.name,
@@ -130,13 +134,17 @@ static int32_t msm_sensor_driver_create_v4l_subdev
 	int32_t rc = 0;
 	uint32_t session_id = 0;
 
+#ifndef CONFIG_MACH_XIAOMI_RIVA
 	if (s_ctrl->bypass_video_node_creation == 0) {
+#endif
 		rc = camera_init_v4l2(&s_ctrl->pdev->dev, &session_id);
 		if (rc < 0) {
 			pr_err("failed: camera_init_v4l2 rc %d", rc);
 			return rc;
 		}
+#ifndef CONFIG_MACH_XIAOMI_RIVA
 	}
+#endif
 
 	CDBG("rc %d session_id %d", rc, session_id);
 	s_ctrl->sensordata->sensor_info->session_id = session_id;
@@ -834,8 +842,10 @@ int32_t msm_sensor_driver_probe(void *setting,
 			slave_info32->sensor_init_params;
 		slave_info->output_format =
 			slave_info32->output_format;
+#ifndef CONFIG_MACH_XIAOMI_RIVA
 		slave_info->bypass_video_node_creation =
 			!!slave_info32->bypass_video_node_creation;
+#endif
 		kfree(slave_info32);
 	} else
 #endif
@@ -878,8 +888,10 @@ int32_t msm_sensor_driver_probe(void *setting,
 		slave_info->sensor_init_params.position);
 	CDBG("mount %d",
 		slave_info->sensor_init_params.sensor_mount_angle);
+#ifndef CONFIG_MACH_XIAOMI_RIVA
 	CDBG("bypass video node creation %d",
 		slave_info->bypass_video_node_creation);
+#endif
 	/* Validate camera id */
 	if (slave_info->camera_id >= MAX_CAMERAS) {
 		pr_err("failed: invalid camera id %d max %d",
@@ -1063,8 +1075,10 @@ CSID_TG:
 
 	pr_err("%s probe succeeded", slave_info->sensor_name);
 
+#ifndef CONFIG_MACH_XIAOMI_RIVA
 	s_ctrl->bypass_video_node_creation =
 		slave_info->bypass_video_node_creation;
+#endif
 
 	/*
 	 * Create /dev/videoX node, comment for now until dummy /dev/videoX
